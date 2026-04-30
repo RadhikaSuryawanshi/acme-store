bba1332b624a3798640c994b99cb59f3b56c8ef2
RadhikaSuryawanshi <radhikasuryawanshi279@gmail.com>
2026-04-30 18:05:37 +0530

    resolve vercel deployment issue

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import ProductPage from "@/Components/ProductPage";
+import { Suspense } from "react";
 
 export default function SearchPage() {
-    return (
-        <div className="w-full ">
-    <ProductPage/>
-        </div>
-    )
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="w-full">
+        <ProductPage />
+      </div>
+    </Suspense>
+  );
 }
